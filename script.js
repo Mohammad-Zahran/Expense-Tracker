b0005e2526d1769a01a8eadd95f967737b472716
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-03 17:21:03 +0200

    Added an icon for deleting transactions with nice style in it

--- script.js
+++ script.js
@@ -37,13 +37,13 @@ function renderList() {
     list.innerHTML = "";
 
     // this condition will check if the dummy transaction is empty
-    if(transactions.length === 0){
+    if (transactions.length === 0) {
         status.textContent = 'No transactions.'
         return;
     }
 
     // The for each here is taking the data and creating new elements when fetching them
-    transactions.forEach(({id, name, amount, date, type }) => {
+    transactions.forEach(({ id, name, amount, date, type }) => {
         const li = document.createElement('li');
 
         li.innerHTML = `
@@ -54,6 +54,12 @@ function renderList() {
 
         <div class="amount ${type}">
            <span>${formatter.format(amount)}</span>
+        </div>
+
+        <div class="action">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
         </div>
 
 
