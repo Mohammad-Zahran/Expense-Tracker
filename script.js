51d0f21575996aee317af3c07a3f3dc95ca9a94d
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-03 15:51:33 +0200

    Testing succesful

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-const transactions = [];
+const transactions = [
+    {
+        id: 1,
+        name: 'salary',
+        amount: 5000,
+        date: new Date(),
+        type: 'income'
+    },
+    {
+        id: 2,
+        name: 'haircut',
+        amount: 20,
+        date: new Date(),
+        type: 'expense'
+    },
+    {
+        id: 3,
+        name: 'ticket',
+        amount: 350,
+        date: new Date(),
+        type: 'expense'
+    },
+
+];
 
 const list = document.getElementById("transactionList");
 
 function renderList() {
     list.innerHTML = "<li>items</li>";
+
+    transactions.forEach((transaction) => {
+        const li = document.createElement('li');
+
+        li.innerHTML = transaction.amount;
+
+        list.appendChild(li)
+    })
 }
 
 renderList();
