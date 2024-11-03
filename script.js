532ee2622388fccdd94086b0a238e7185a469a1f
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-03 21:35:11 +0200

    Made the filter system

--- script.js
+++ script.js
@@ -22,13 +22,11 @@ const transactions = [
     },
 ];
 
-// The formatter variable is an instance of Intl.NumberFormat, which is part of JavaScript's Intl (Internationalization) API. It’s used to format numbers as currency, in this case, US dollars. Here’s what each option does:
-
 const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     signDisplay: 'always',
-})
+});
 
 const list = document.getElementById("transactionList");
 const status = document.getElementById('status');
@@ -36,69 +34,60 @@ const form = document.getElementById('transactionForm');
 const balance = document.getElementById('balance');
 const income = document.getElementById('income');
 const expense = document.getElementById('expense');
-
+const filterForm = document.getElementById('filterForm');
 
 form.addEventListener('submit', addTransaction);
+filterForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    filterTransactions();
+});
 
-// function calculates the total income, total expenses, and balance, and then formats and updates these values in the respective HTML elements on the page.
 function updateTotal() {
     const incomeTotal = transactions
-    .filter((trx) => trx.type ==='income')
-    .reduce((total, trx) => total + trx.amount, 0);
+        .filter((trx) => trx.type === 'income')
+        .reduce((total, trx) => total + trx.amount, 0);
 
     const expenseTotal = transactions
-    .filter((trx) => trx.type ==='expense')
-    .reduce((total, trx) => total + trx.amount, 0);
+        .filter((trx) => trx.type === 'expense')
+        .reduce((total, trx) => total + trx.amount, 0);
 
     const balanceTotal = incomeTotal - expenseTotal;
 
     balance.textContent = formatter.format(balanceTotal).substring(1);
     income.textContent = formatter.format(incomeTotal);
     expense.textContent = formatter.format(expenseTotal * -1);
-
 }
+
 
 function renderList() {
     list.innerHTML = "";
 
-    // this condition will check if the dummy transaction is empty
     if (transactions.length === 0) {
-        status.textContent = 'No transactions.'
+        status.textContent = 'No transactions.';
         return;
-    }
-    else{
+    } else {
         status.textContent = "";
     }
 
-    // The for each here is taking the data and creating new elements when fetching them
     transactions.forEach(({ id, name, amount, date, type }) => {
-
-        const sign = 'income' === type ? 1: -1;
+        const sign = 'income' === type ? 1 : -1;
 
         const li = document.createElement('li');
-
         li.innerHTML = `
-        <div class="name">
-           <h4>${name}</h4>
-           <p>${new Date(date).toLocaleDateString()}</p>
-        </div>
-
-        <div class="amount ${type}">
-           <span>${formatter.format(amount * sign)}</span>
-        </div>
-
-        
-        <div class="action">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="size-6" onclick="deleteTransaction(${id})">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-            </svg>
-        </div>
-
+            <div class="name">
+                <h4>${name}</h4>
+                <p>${new Date(date).toLocaleDateString()}</p>
+            </div>
+            <div class="amount ${type}">
+                <span>${formatter.format(amount * sign)}</span>
+            </div>
+            <div class="action">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onclick="deleteTransaction(${id})">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+            </div>
         `;
-
-        list.appendChild(li)
+        list.appendChild(li);
     });
 }
 
@@ -107,36 +96,77 @@ updateTotal();
 
 function deleteTransaction(id) {
     const index = transactions.findIndex((trx) => trx.id === id);
-    transactions.splice(index,1);
+    transactions.splice(index, 1);
 
     updateTotal();
     renderList();
 }
 
-// This function will make the user add the new data and submit it without reloading the page
-function addTransaction(e){
-    // I used e.preventDefault() to not reload the page when pressing the submit
+function addTransaction(e) {
     e.preventDefault();
 
     const formData = new FormData(this);
 
-    // This will push the new transaction
     transactions.push({
         id: transactions.length + 1,
         name: formData.get("name"),
         amount: parseFloat(formData.get("amount")),
         date: new Date(formData.get('date')),
-        type: "on" === formData.get("type") ? "income" : "expense"
+        type: formData.get("type") ? "income" : "expense"
     });
 
     this.reset();
-
     updateTotal();
     renderList();
 }
 
-function saveTransactions() {
-    transactions.sort((a,b) => new Date(b.date) - new Date(a.date))
-    localStorage.setItem('transactions', JSON.stringify(transactions));
+function filterTransactions() {
+    const minAmount = parseFloat(filterForm.minAmount.value) || 0;
+    const maxAmount = parseFloat(filterForm.maxAmount.value) || Infinity;
+    const type = filterForm.type.value;
+    const date = new Date(filterForm.date.value);
+    const note = filterForm.note.value.toLowerCase();
 
+    const filteredTransactions = transactions.filter(trx => {
+        const matchesAmount = trx.amount >= minAmount && trx.amount <= maxAmount;
+        const matchesType = type ? trx.type === type : true;
+        const matchesDate = date.toString() !== "Invalid Date" ? new Date(trx.date).toDateString() === date.toDateString() : true;
+        const matchesNote = trx.name.toLowerCase().includes(note);
+        
+        return matchesAmount && matchesType && matchesDate && matchesNote;
+    });
+
+    renderFilteredList(filteredTransactions);
+}
+
+function renderFilteredList(filteredTransactions) {
+    list.innerHTML = "";
+    
+    if (filteredTransactions.length === 0) {
+        status.textContent = 'No transactions match the filter criteria.';
+        return;
+    } else {
+        status.textContent = "";
+    }
+
+    filteredTransactions.forEach(({ id, name, amount, date, type }) => {
+        const sign = 'income' === type ? 1 : -1;
+
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <div class="name">
+                <h4>${name}</h4>
+                <p>${new Date(date).toLocaleDateString()}</p>
+            </div>
+            <div class="amount ${type}">
+                <span>${formatter.format(amount * sign)}</span>
+            </div>
+            <div class="action">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onclick="deleteTransaction(${id})">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+            </div>
+        `;
+        list.appendChild(li);
+    });
 }
