1f40e0c67d2c5d8a6f85deddf1003af9fb10ec53
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-08 11:18:02 +0200

    Created the logic of the login.php

--- login.php
+++ login.php
@@ -1,5 +1,34 @@
 <?php
 
-echo "Hello"
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+    function validate($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 
+    $uname = validate($_POST['uname']);
+    $pass = validate($_POST['password']);
+
+    if (empty($uname)) {
+        header("Location: login.html?error=User Name is required");
+        exit();
+    }
+    else if (empty($pass)) {
+        header("Location: login.html?error=Password is required");
+        exit();
+    } 
+    else {
+        header("Location: index.html");
+        exit();
+    }
+} else {
+    header("Location: login.html");
+    exit();
+}
+
+if ($isset($_GET['error'])){
+    
+}
 ?>
